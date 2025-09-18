4d6af7b576e54e744fd113d7c36b3c0b1100f152
Pablo Carrazana <carrazanap@gmail.com>
2025-09-18 19:20:13 -0300

    Create CrearAutomovilCommand.cs

---
+++ Backend/Application/UseCases/Automovil/Commands/CrearAutomovil/CrearAutomovilCommand.cs
@@ -0,0 +1,24 @@
+using Core.Application.ComandQueryBus.Commands;
+
+namespace Application.UseCases.Automovil.Commands.CrearAutomovil
+{
+    public class CrearAutomovilCommand : IRequestCommand<int>
+    {
+        public string Marca { get; set; } = string.Empty;
+        public string Modelo { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public int Fabricacion { get; set; }
+        public string NumeroMotor { get; set; } = string.Empty;
+        public string NumeroChasis { get; set; } = string.Empty;
+
+        public CrearAutomovilCommand(string marca, string modelo, string color, int fabricacion, string numeroMotor, string numeroChasis)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Color = color;
+            Fabricacion = fabricacion;
+            NumeroMotor = numeroMotor;
+            NumeroChasis = numeroChasis;
+        }
+    }
+}
