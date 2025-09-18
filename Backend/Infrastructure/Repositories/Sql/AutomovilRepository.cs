b173ad82b43652a60dc4972222703467b0751bb2
Pablo Carrazana <carrazanap@gmail.com>
2025-09-18 19:22:44 -0300

    Create AutomovilRepository.cs

---
+++ Backend/Infrastructure/Repositories/Sql/AutomovilRepository.cs
@@ -0,0 +1,47 @@
+using Application.Repositories;
+using Core.Infraestructure.Repositories.Sql;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Sql
+{
+    internal class AutomovilRepository : BaseRepository<Automovil>, IAutomovilRepository
+    {
+        public AutomovilRepository(StoreDbContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> ExisteNumeroMotorAsync(string numeroMotor, int? excluirId = null)
+        {
+            return await _context.Set<Automovil>()
+                .AnyAsync(a => a.NumeroMotor == numeroMotor && (excluirId == null || a.Id != excluirId));
+        }
+
+        public async Task<bool> ExisteNumeroChasisAsync(string numeroChasis, int? excluirId = null)
+        {
+            return await _context.Set<Automovil>()
+                .AnyAsync(a => a.NumeroChasis == numeroChasis && (excluirId == null || a.Id != excluirId));
+        }
+
+        public async Task<IEnumerable<Automovil>> BuscarPorMarcaAsync(string marca)
+        {
+            return await _context.Set<Automovil>()
+                .Where(a => a.Marca.Contains(marca))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Automovil>> BuscarPorAnioFabricacionAsync(int anioInicio, int anioFin)
+        {
+            return await _context.Set<Automovil>()
+                .Where(a => a.Fabricacion >= anioInicio && a.Fabricacion <= anioFin)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Automovil>> BuscarPorColorAsync(string color)
+        {
+            return await _context.Set<Automovil>()
+                .Where(a => a.Color.Contains(color))
+                .ToListAsync();
+        }
+    }
+}
