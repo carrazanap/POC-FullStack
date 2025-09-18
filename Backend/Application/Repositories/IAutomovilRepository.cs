9b5a5ecca1e6e91a1d7cb809fb008d3773efc401
Pablo Carrazana <carrazanap@gmail.com>
2025-09-18 19:15:56 -0300

    Create IAutomovilRepository.cs

---
+++ Backend/Application/Repositories/IAutomovilRepository.cs
@@ -0,0 +1,14 @@
+using Core.Application.Repositories;
+using Domain.Entities;
+
+namespace Application.Repositories
+{
+    public interface IAutomovilRepository : IRepository<Automovil, int>
+    {
+        Task<bool> ExisteNumeroMotorAsync(string numeroMotor, int? excluirId = null);
+        Task<bool> ExisteNumeroChasisAsync(string numeroChasis, int? excluirId = null);
+        Task<IEnumerable<Automovil>> BuscarPorMarcaAsync(string marca);
+        Task<IEnumerable<Automovil>> BuscarPorAnioFabricacionAsync(int anioInicio, int anioFin);
+        Task<IEnumerable<Automovil>> BuscarPorColorAsync(string color);
+    }
+}
