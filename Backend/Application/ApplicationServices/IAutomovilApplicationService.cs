e8c0b766f3ac9edc603438b2c6b45de4d6714baf
Pablo Carrazana <carrazanap@gmail.com>
2025-09-18 19:17:24 -0300

    Create IAutomovilApplicationService.cs

---
+++ Backend/Application/ApplicationServices/IAutomovilApplicationService.cs
@@ -0,0 +1,18 @@
+using Application.DataTransferObjects;
+
+namespace Application.ApplicationServices
+{
+    public interface IAutomovilApplicationService
+    {
+        Task<IEnumerable<AutomovilDto>> ObtenerTodosAsync();
+        Task<AutomovilDto?> ObtenerPorIdAsync(int id);
+        Task<int> CrearAsync(CrearAutomovilDto dto);
+        Task<bool> ActualizarAsync(int id, ActualizarAutomovilDto dto);
+        Task<bool> EliminarAsync(int id);
+        Task<IEnumerable<AutomovilDto>> BuscarPorMarcaAsync(string marca);
+        Task<IEnumerable<AutomovilDto>> BuscarPorAnioFabricacionAsync(int anioInicio, int anioFin);
+        Task<IEnumerable<AutomovilDto>> BuscarPorColorAsync(string color);
+        Task<bool> ValidarNumeroMotorUnicoAsync(string numeroMotor, int? excluirId = null);
+        Task<bool> ValidarNumeroChasisUnicoAsync(string numeroChasis, int? excluirId = null);
+    }
+}
